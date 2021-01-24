1e94359e3d65183c2438e8471c7af839560aa0c6
Rosen Toshev <rosen.toshev@expend.io>
2021-01-24 16:37:55 +0200

    Added button color and onClick change.

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import {replaceCamelWithSpaces} from './App.js'
 
-test('renders learn react link', () => {
+test('button has correct intitial color', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn testing library/i);
-  // expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+ // expect the background color to be red
+ expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 });
 
-describe('spaces before camel-case capital letters', () => {
-  test('Works for no inner capital letters', () => {
-    expect(replaceCamelWithSpaces('Red')).toBe('Red');
-  });
-  test('Works for one inner capital letter', () => {
-    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
-  });
-  test('Works for multiple capital letters', () => {
-    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
-  })
-})
+test('button turns blue when clicked', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+
+  // expect the button text to 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
+});
+
+
+test('the button has correct initial text', () => {
+  
+});
